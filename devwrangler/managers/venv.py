4593c21cea437058cdcf2a72a5c97286d86fff4e
Nick Cannariato <434063+birdcar@users.noreply.github.com>
2021-11-16 22:42:31 -0600

    Create the venv-based manager, based on the base manager API

---
+++ devwrangler/managers/venv.py
@@ -0,0 +1,32 @@
+"""Implement a venv specific manager for those who prefer to use venv."""
+import venv
+from pathlib import Path
+
+from .base import BaseManager
+
+
+class VenvManager(BaseManager):
+    """Manage virtual environments with Python's 'venv' module."""
+
+    def create(self):
+        """Create a virtual environment using venv."""
+        venv.create(
+            self.venv_path,
+            with_pip=True,
+            prompt=self.venv_path.parent.name,
+        )
+
+    def install_dependencies(self, quiet: bool = True):
+        """Install environment dependencies."""
+        minimal_dependencies = [
+            str(self.prefix),
+            "-m",
+            "pip",
+            "install",
+            "-U",
+            "pip",
+            "setuptools",
+        ]
+        if quiet:
+            minimal_dependencies.append("-qqq")
+        self.cmd(minimal_dependencies)
